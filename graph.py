a66fcb2ba9f29b75d160278c396cb63841e8cb4b
flheight <75139846+flheight@users.noreply.github.com>
2024-06-02 23:24:01 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -6,13 +6,44 @@ class Graph:
     def __init__(self, n_classes):
         self.n_classes = n_classes
 
-    def fit(self, X, n_nodes):
-        centers = KMeans(n_clusters=n_nodes).fit(X).cluster_centers_
+    def fit(self, X):
+        max_iter = int(np.log2(X.shape[0]))
+        kmeans = [KMeans(n_clusters=2**i).fit(X) for i in range(max_iter)]
+        bic = [kmeans[i].inertia_ + .5 * (2**i + 1) * np.log(X.shape[0]) for i in range(max_iter)]
 
-        dist = pairwise_distances(centers)
+        best_kmeans = kmeans[np.argmin(bic) + 1]
+        centers = best_kmeans.cluster_centers_
+
+        dist = 20*pairwise_distances(centers, metric='sqeuclidean')
         np.fill_diagonal(dist, np.inf)
 
-        affinity = np.exp(dist / -np.std(dist.min(axis=1)))
+        guess = best_kmeans.predict(X)
+        
+        for i in range(dist.shape[0]):
+            data_i = X[guess == i]
+            for j in range(i):
+                points_ij = np.vstack((centers[i], centers[j]))
+                data_j = X[guess == j]
+                data = np.vstack((data_i, data_j))
+
+                diffs = data - points_ij[:, np.newaxis]
+                dists = np.min(np.einsum('ijk,ijk->ij', diffs, diffs), axis=0)
+                intertia_points = dists.mean()
+                
+                segment = (centers[j] - centers[i]).reshape(1, -1)
+                projs = np.dot(data - centers[i], segment.T) / np.einsum('ij,ij->i', segment, segment)
+                nearests = centers[i] + np.outer(np.clip(projs, 0, 1), segment)
+                diffs = data - nearests
+                dists = np.einsum('ij,ij->i', diffs, diffs)
+                intertia_segment = dists.mean()
+
+                dist[i, j] += intertia_segment - intertia_points
+
+        dist = dist + dist.T
+
+        print(dist)
+
+        affinity = np.exp(dist / -np.var(dist.min(axis=1)))
         
         labels = SpectralClustering(n_clusters=self.n_classes, affinity='precomputed').fit_predict(affinity)
 
