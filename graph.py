f846c189bfc69ebf9fb87ca1416f9c5d0d20cd2b
flheight <75139846+flheight@users.noreply.github.com>
2024-06-02 23:27:17 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -11,39 +11,13 @@ class Graph:
         kmeans = [KMeans(n_clusters=2**i).fit(X) for i in range(max_iter)]
         bic = [kmeans[i].inertia_ + .5 * (2**i + 1) * np.log(X.shape[0]) for i in range(max_iter)]
 
-        best_kmeans = kmeans[np.argmin(bic) + 1]
+        best_kmeans = kmeans[np.minimum(np.argmin(bic) + 1, max_iter - 1)]
         centers = best_kmeans.cluster_centers_
 
-        dist = 20*pairwise_distances(centers, metric='sqeuclidean')
+        dist = pairwise_distances(centers, metric='sqeuclidean')
         np.fill_diagonal(dist, np.inf)
 
-        guess = best_kmeans.predict(X)
-        
-        for i in range(dist.shape[0]):
-            data_i = X[guess == i]
-            for j in range(i):
-                points_ij = np.vstack((centers[i], centers[j]))
-                data_j = X[guess == j]
-                data = np.vstack((data_i, data_j))
-
-                diffs = data - points_ij[:, np.newaxis]
-                dists = np.min(np.einsum('ijk,ijk->ij', diffs, diffs), axis=0)
-                intertia_points = dists.mean()
-                
-                segment = (centers[j] - centers[i]).reshape(1, -1)
-                projs = np.dot(data - centers[i], segment.T) / np.einsum('ij,ij->i', segment, segment)
-                nearests = centers[i] + np.outer(np.clip(projs, 0, 1), segment)
-                diffs = data - nearests
-                dists = np.einsum('ij,ij->i', diffs, diffs)
-                intertia_segment = dists.mean()
-
-                dist[i, j] += intertia_segment - intertia_points
-
-        dist = dist + dist.T
-
-        print(dist)
-
-        affinity = np.exp(dist / -np.var(dist.min(axis=1)))
+        affinity = np.exp(dist / -np.std(dist.min(axis=1)))
         
         labels = SpectralClustering(n_clusters=self.n_classes, affinity='precomputed').fit_predict(affinity)
 
